c33cb819437d44074ce19d6dba88256c60ed74fa
Jamie Lam <JamieLam@Jamies-iMac.local>
2017-09-24 19:01:03 -0600

    updated page 2.php

---
+++ Assignment 1/whoAmI.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!-- Jamie Lam, age 28 -->
+<html lang = "en"> 
+    <head>
+        <meta name= "viewport" content = "width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <?php
+        ?>        
+    </body>
+</html>
