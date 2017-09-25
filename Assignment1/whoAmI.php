f04cdf791fc48ab860a39d5f2eb9bb93418d3994
Jamie Lam <JamieLam@Jamies-iMac.local>
2017-09-24 21:17:20 -0600

    updated css

--- Assignment1/whoAmI.php
+++ Assignment1/whoAmI.php
@@ -5,7 +5,7 @@
         <meta name= "viewport" content = "width=device-width, initial-scale=1">
     </head>
     <style>
-        radio.first radio.Male {
+        li.first radio.Male {
             background-color: #B3E5FC;
         }
     </style>
