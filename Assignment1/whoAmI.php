5b3566aecf90b79675887e76d0cbc535adbbd6d9
Jamie Lam <JamieLam@Jamies-iMac.local>
2017-09-25 20:42:46 -0600

    added css back in

--- Assignment1/whoAmI.php
+++ Assignment1/whoAmI.php
@@ -5,9 +5,9 @@
         <meta name= "viewport" content = "width=device-width, initial-scale=1">
     </head>
     <style>
-        /* select.Male {
+        select.Male {
             background-color: #B3E5FC;
-        } */
+        }
     </style>
     <body>
         <form action="whoYouAre.php" method="POST">
