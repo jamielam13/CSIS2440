950aeeef9565e541e47b0daee55325aca16c2955
Jamie Lam <JamieLam@Jamies-iMac.local>
2017-09-25 21:09:34 -0600

    updated for male bg color only

--- Assignment1/whoYouAre.php
+++ Assignment1/whoYouAre.php
@@ -23,7 +23,7 @@
             printf($person);
             echo "<br>";
 
-            if ($gender = "Male") {
+            if ($gender == "Male") {
                 echo '<body style = "background-color:#B3E5FC">';
             } else  {
                 echo '<body style = "background-color:white">';
