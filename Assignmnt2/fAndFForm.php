4ea4025711ea7ad6398eb61d925410d84ff0c3fa
Jamie Lam <JamieLam@Jamies-iMac.local>
2017-10-23 22:08:45 -0600

    added new assignment

---
+++ Assignmnt2/fAndFForm.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!-- Jamie Lam, age 28 -->
+<html lang = "en"> 
+    <head>
+        <meta name= "viewport" content = "width=device-width, initial-scale=1">
+    </head>
+    <style>
+    </style>
+    <body>
+        <h1>Fill out the form</h1>
+
+        <!-- form -->
+        <form action="results.php" method="POST">
+           
+        </form>      
+    </body>
+</html>
