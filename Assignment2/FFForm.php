07d7b99266613d66a4e07795b9c1f0dbc9dfe387
Jamie Lam <JamieLam@Jamies-iMac.local>
2017-10-28 21:33:33 -0600

    updated button position 1

--- Assignment2/FFForm.php
+++ Assignment2/FFForm.php
@@ -266,12 +266,13 @@
                     </tr>
                     <tr>
                         <td>&nbsp;</td>
-                        <td><input id="button_Search" type="submit" value="Search" name="requestTypeSearch"></td>
+                        <td><input id="button_Search" type="submit" value="Search" name="requestTypeSearch"/></td>
+                        <td><input id="Button_update" type="submit" value="Update" name="requestTypeUpdate"/></td>
                         <td>
                             <table>
                                 <tr>
                                     <td><input id="button_Search" type="submit" value="Search" name="requestTypeSearch"/></td>
-                                    <td><input id="Button_update" type="submit" value="Update" name="requestTypeUpdate"/></td>
+                                    
                                     <td><input id="Button_Input" type="submit" value="Create" name="requestTypeCreate"/></td>
                                 </tr>
                             </table>
