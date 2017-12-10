f173e4fb66c6d8fc052c730ae73e910c098b8a17
Jamie Lam <JamieLam@Jamies-iMac.local>
2017-12-10 11:58:44 -0700

    added regex back in

--- Assignment3/FFFormValidation.php
+++ Assignment3/FFFormValidation.php
@@ -9,11 +9,11 @@
             //form validation
             function validateForm() {
                 var nameRegex = /^[a-zA-Z]+$/;
-                // if (nameRegex.test(fname.value)== false) {
-                //     alert("First name must include letters only!")
-                //     document.myForm.fname.focus();
-                //     return false;
-                // }
+                if (nameRegex.test(fname.value)== false) {
+                    alert("First name must include letters only!")
+                    document.myForm.fname.focus();
+                    return false;
+                }
                 if (document.myForm.fname.value == "")  {
                     alert("First Name must be entered!");
                     document.myForm.fname.focus();
