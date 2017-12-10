d4bab4092a543b6dc99f0bd8a1fa95a0333fe2c1
Jamie Lam <JamieLam@Jamies-iMac.local>
2017-12-10 16:29:09 -0700

    update

--- Assignment3/FFFormValidation.php
+++ Assignment3/FFFormValidation.php
@@ -13,35 +13,36 @@
 
                 if (document.myForm.value = "") {
                     alert("Form must be completed!!");
-                    return false;
-                } 
-                if (nameRegex.test(fname.value)== false && document.myForm.fname.value == "") {
+
+                    if (nameRegex.test(fname.value)== false && document.myForm.fname.value == "") {
                         alert("First name required and must include letters only!")
                         document.myForm.fname.focus();
                         return false;
                     }
-                if (nameRegex.test(fname.value)== false )  {
+                     if (nameRegex.test(fname.value)== false )  {
                         alert("First Name must include letters only!");
                         document.myForm.fname.focus();
                         return false;
                     }
-                    
-                if (nameRegex.test(lname.value)== false && document.myForm.lname.value == "") {
+                    if (nameRegex.test(lname.value)== false && document.myForm.lname.value == "") {
                         alert("Last name required and must include letters only!")
                         document.myForm.lname.focus();
                         return false;
                     }
-                if (nameRegex.test(lname.value)== false)  {
+                    if (nameRegex.test(lname.value)== false)  {
                         alert("Last Name must include letters only!");
                         document.myForm.lname.focus();
                         return false;
                     }
                     //phone number validation
-                if (phoneRegex.test(phone.value)== false) {
+                    if (phoneRegex.test(phone.value)== false) {
                         alert("Phone cannot be blank and must include numbers only!");
                         document.myForm.phone.focus();
                         return false;
                     }
+
+                    return false;
+                } 
             }
         </script>
     </head>
