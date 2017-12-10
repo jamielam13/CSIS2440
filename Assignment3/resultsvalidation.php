cff3a2bd4d233a2af6fb0b7a2c1b1438ea154852
Jamie Lam <JamieLam@Jamies-iMac.local>
2017-12-10 11:48:23 -0700

    updated resultsvalidation.php

--- Assignment3/resultsvalidation.php
+++ Assignment3/resultsvalidation.php
@@ -38,127 +38,127 @@
 
             echo "<br>";
 
-            $sql =  'CREATE TABLE FFVALIDATION (
-                        ID  SERIAL PRIMARY KEY,
-                        FirstName   TEXT    NOT NULL,
-                        LastName    TEXT    NOT NULL,
-                        PhoneNumber TEXT    NOT NULL,
-                        Address     TEXT    NOT NULL,
-                        City        TEXT    NOT NULL,
-                        State       TEXT    NOT NULL,
-                        ZIP         INT     NOT NULL,
-                        UserName    TEXT    NOT NULL,
-                        Password    TEXT    NOT NULL,
-                        Gender      TEXT    NOT NULL,
-                        Relation    TEXT    NOT NULL
-                    )';
+            // $sql =  'CREATE TABLE FF (
+            //             ID  SERIAL PRIMARY KEY,
+            //             FirstName   TEXT    NOT NULL,
+            //             LastName    TEXT    NOT NULL,
+            //             PhoneNumber TEXT    NOT NULL,
+            //             Address     TEXT    NOT NULL,
+            //             City        TEXT    NOT NULL,
+            //             State       TEXT    NOT NULL,
+            //             ZIP         INT     NOT NULL,
+            //             UserName    TEXT    NOT NULL,
+            //             Password    TEXT    NOT NULL,
+            //             Gender      TEXT    NOT NULL,
+            //             Relation    TEXT    NOT NULL
+            //         )';
             $firstTable = true;
-            echo "table created";
-            // if($_POST){
-            //     if($_POST['requestTypeCreate']){
-            //         $sql = "INSERT INTO FF(FirstName, LastName, PhoneNumber, Address, City, State, ZIP, UserName, Password, Gender, Relation) 
-            //                 VALUES('$firstName', '$lastName', '$phoneNumber', '$address', '$city', '$state', '$zip', '$userName', '$password', '$gender', '$relation')";
 
-            //         $ret = pg_query($db, $sql);
-            //         if(!$ret){
-            //             echo pg_last_error($db);
-            //         }
-            //         else {
-            //             echo "Insert was successful! \n";
-            //         }
-            //     }
-            //     else if($_POST['requestTypeUpdate']){
-            //         $sql = "UPDATE FF SET FirstName = '$firstName', LastName = '$lastName', Address = '$address', City = '$city', State = '$state', ZIP = '$zip', UserName = '$userName', Password = '$password', Gender = '$gender', Relation = '$relation'
-            //                 WHERE PhoneNumber = '$phoneNumber';";
+            if($_POST){
+                if($_POST['requestTypeCreate']){
+                    $sql = "INSERT INTO FF(FirstName, LastName, PhoneNumber, Address, City, State, ZIP, UserName, Password, Gender, Relation) 
+                            VALUES('$firstName', '$lastName', '$phoneNumber', '$address', '$city', '$state', '$zip', '$userName', '$password', '$gender', '$relation')";
 
-            //         $ret = pg_query($db, $sql);
-            //         if(!$ret){
-            //             echo pg_last_error($db);
-            //         }
-            //         else {
-            //             echo "Update was successful! \n";
-            //         }
-            //     }
-            //     else if($_POST['requestTypeSearch']){
+                    $ret = pg_query($db, $sql);
+                    if(!$ret){
+                        echo pg_last_error($db);
+                    }
+                    else {
+                        echo "Insert was successful! \n";
+                    }
+                }
+                else if($_POST['requestTypeUpdate']){
+                    $sql = "UPDATE FF SET FirstName = '$firstName', LastName = '$lastName', Address = '$address', City = '$city', State = '$state', ZIP = '$zip', UserName = '$userName', Password = '$password', Gender = '$gender', Relation = '$relation'
+                            WHERE PhoneNumber = '$phoneNumber';";
 
-            //         $sql = "SELECT FF.FirstName, FF.LastName, FF.PhoneNumber, FF.Address, FF.City, FF.State, FF.ZIP, FF.Relation 
-            //                 FROM FF
-            //                 WHERE FirstName = '$firstName' /*OR LastName = '$lastName' OR Address = '$address' OR City = '$city' OR State = '$state' OR ZIP = '$zip' OR UserName = '$userName' OR Password = '$password' OR Gender = '$gender' OR Relation = '$relation'*/;";
+                    $ret = pg_query($db, $sql);
+                    if(!$ret){
+                        echo pg_last_error($db);
+                    }
+                    else {
+                        echo "Update was successful! \n";
+                    }
+                }
+                else if($_POST['requestTypeSearch']){
 
-            //         $ret = pg_query($db, $sql);
-            //         if(pg_num_rows($ret) == 0){
-            //             echo "Zero rows returned from search...";
-            //         }
-            //         else{
-            //             if(!$ret){
-            //                 echo pg_last_error($db);
-            //             }
-            //             else {
-            //                 echo "Search was successful! \n";
-            //             }
-            //         }
+                    $sql = "SELECT FF.FirstName, FF.LastName, FF.PhoneNumber, FF.Address, FF.City, FF.State, FF.ZIP, FF.Relation 
+                            FROM FF
+                            WHERE FirstName = '$firstName' /*OR LastName = '$lastName' OR Address = '$address' OR City = '$city' OR State = '$state' OR ZIP = '$zip' OR UserName = '$userName' OR Password = '$password' OR Gender = '$gender' OR Relation = '$relation'*/;";
 
-            //         while($row = pg_fetch_row($ret)){
-            //             $table = "<table>
-            //                             <tr>
-            //                                 <th>Firstname</th>
-            //                                 <th>Lastname</th>
-            //                                 <th>Address</th>
-            //                                 <th>City</th>
-            //                                 <th>State</th>
-            //                                 <th>Zip</th>
-            //                                 <th>Phone</th>
-            //                                 <th>Relation</th>
-            //                             </tr>
-            //                             <tr>
-            //                                 <td>$row[0]</td>
-            //                                 <td>$row[1]</td>
-            //                                 <td>$row[6]</td>
-            //                                 <td>$row[3]</td>
-            //                                 <td>$row[4]</td>
-            //                                 <td>$row[5]</td>
-            //                                 <td>$row[2]</td>
-            //                                 <td>$row[7]</td>
-            //                                 <td>$row[8]</td>
-            //                             </tr>
-            //                     </table>";
-            //             printf($table);
-            //         }
+                    $ret = pg_query($db, $sql);
+                    if(pg_num_rows($ret) == 0){
+                        echo "Zero rows returned from search...";
+                    }
+                    else{
+                        if(!$ret){
+                            echo pg_last_error($db);
+                        }
+                        else {
+                            echo "Search was successful! \n";
+                        }
+                    }
 
-            //         $firstTable = false;
-            //     }
-            // }
+                    while($row = pg_fetch_row($ret)){
+                        $table = "<table>
+                                        <tr>
+                                            <th>Firstname</th>
+                                            <th>Lastname</th>
+                                            <th>Address</th>
+                                            <th>City</th>
+                                            <th>State</th>
+                                            <th>Zip</th>
+                                            <th>Phone</th>
+                                            <th>Relation</th>
+                                        </tr>
+                                        <tr>
+                                            <td>$row[0]</td>
+                                            <td>$row[1]</td>
+                                            <td>$row[6]</td>
+                                            <td>$row[3]</td>
+                                            <td>$row[4]</td>
+                                            <td>$row[5]</td>
+                                            <td>$row[2]</td>
+                                            <td>$row[7]</td>
+                                            <td>$row[8]</td>
+                                        </tr>
+                                </table>";
+                        printf($table);
+                    }
 
-            // if($firstTable){
-            //     $table = "<table>
-            //                 <tr>
-            //                     <th>Firstname</th>
-            //                     <th>Lastname</th>
-            //                     <th>Address</th>
-            //                     <th>City</th>
-            //                     <th>State</th>
-            //                     <th>Zip</th>
-            //                     <th>Phone</th>
-            //                     <th>Relation</th>
-            //                 </tr>
-            //                 <tr>
-            //                     <td>$firstName</td>
-            //                     <td>$lastName</td>
-            //                     <td>$address</td>
-            //                     <td>$city</td>
-            //                     <td>$state</td>
-            //                     <td>$zip</td>
-            //                     <td>$phoneNumber</td>
-            //                     <td>$relation</td>
-            //                 </tr>
-            //             </table>";
+                    $firstTable = false;
+                }
+            }
 
-            //     printf($table);
-            // }
+            if($firstTable){
+                $table = "<table>
+                            <tr>
+                                <th>Firstname</th>
+                                <th>Lastname</th>
+                                <th>Address</th>
+                                <th>City</th>
+                                <th>State</th>
+                                <th>Zip</th>
+                                <th>Phone</th>
+                                <th>Relation</th>
+                            </tr>
+                            <tr>
+                                <td>$firstName</td>
+                                <td>$lastName</td>
+                                <td>$address</td>
+                                <td>$city</td>
+                                <td>$state</td>
+                                <td>$zip</td>
+                                <td>$phoneNumber</td>
+                                <td>$relation</td>
+                            </tr>
+                        </table>";
+
+                printf($table);
+            }
 
             
-            // echo"<br>";
-            // pg_close($db);
+            echo"<br>";
+            pg_close($db);
          ?>     
     </body>
 </html>
